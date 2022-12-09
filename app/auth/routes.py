02df55408f24d579b2e28447f58b9712e43fb40e
SeanECurrie <101378600+SeanECurrie@users.noreply.github.com>
2022-12-09 17:50:12 -0600

    UPDATE

---
+++ app/auth/routes.py
@@ -0,0 +1,57 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_user, logout_user, current_user
+from app.auth.forms import UserCreationForm, LoginForm
+from app.models import User
+from werkzeug.security import check_password_hash
+
+auth = Blueprint('auth', __name__, template_folder='auth_templates')
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = UserCreationForm()
+    if request.method == 'POST':
+        if form.validate():
+            username = form.username.data
+            email = form.email.data
+            password = form.password.data
+
+            print(username, email, password)
+
+            #instantiate User from models
+            user = User(username, email, password)
+
+            #add user to database
+            # db.session.add(user)
+            # db.session.commit()
+            user.save_to_db()
+
+            return redirect(url_for('auth.login'))
+            
+            
+    return render_template('signup.html', form=form)
+
+@auth.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if request.method=='POST':
+        if form.validate():
+            username = form.username.data
+            password = form.password.data
+
+            #Query user from db
+            user = User.query.filter_by(username=username).first()
+            if user:
+                
+                if check_password_hash(user.password, password):
+                    print('Logged In')
+                    login_user(user)
+                else:
+                    print('Invalid Password')
+            else:
+                print('user does not exist')
+    return render_template('login.html', form=form)
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
