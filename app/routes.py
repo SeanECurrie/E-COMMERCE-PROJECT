268c43a5c7c6c9066220f5f278bef1b11c5fa145
nickd626 <ndonato626@gmail.com>
2022-12-10 18:44:39 -0500

    add cart page and functionality

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
+from app.models import Cart, User
 from flask import render_template
 import requests
+from flask_login import current_user
 
 def get_driver_info(productName):
     productData = []
@@ -52,3 +54,20 @@ def productView(id):
     rating = getAllProducts.json()[id]['rating']
 
     return render_template('single-product-view.html', name=name, price=price, description=description, category=category, image=image, rating=rating)
+
+@app.route('/cart/<int:id>', methods=['GET', 'POST'])
+def cart(id):
+    allProducts = 'https://fakestoreapi.com/products'
+    getAllProducts = requests.get(allProducts)
+
+    title = getAllProducts.json()[id]['title']
+    price = getAllProducts.json()[id]['price']
+    image = getAllProducts.json()[id]['image']
+    qty = 1
+
+    cart = Cart(title=title, image=image, price=price, qty=qty, user_id=current_user.id)
+    Cart.save_to_db(cart)
+
+    user_cart = Cart.query.filter_by(user_id = current_user.id)
+
+    return render_template('cart.html', title=title, image=image, price=price, qty=qty, user_cart=user_cart)
